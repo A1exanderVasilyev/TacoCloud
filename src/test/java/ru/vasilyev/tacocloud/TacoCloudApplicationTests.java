9f45055026cf0dde523d6a933571e551bb405701
Catina Swag <catinasw@va-i2.kzn.21-school.ru>
2025-10-03 13:58:48 +0300

    init

---
+++ src/test/java/ru/vasilyev/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vasilyev.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
