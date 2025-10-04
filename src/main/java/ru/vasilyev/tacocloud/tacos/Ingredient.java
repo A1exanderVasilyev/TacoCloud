de052a241983cc4fd3ceca0644d3b8081fca4fd2
catinasw <you@example.com>
2025-10-04 15:16:04 +0300

    made Ingredient class

---
+++ src/main/java/ru/vasilyev/tacocloud/tacos/Ingredient.java
@@ -0,0 +1,18 @@
+package ru.vasilyev.tacocloud.tacos;
+
+import lombok.Data;
+
+@Data
+public class Ingredient {
+    private final int id;
+    private final String name;
+    private final Type type;
+
+    public enum Type {
+        WRAP,
+        PROTEIN,
+        VEGGIES,
+        CHEESE,
+        SAUCE
+    }
+}
